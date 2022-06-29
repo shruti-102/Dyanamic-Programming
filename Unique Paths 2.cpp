879a682e4c5d0ae6ebe9fcadfbdfecedf3cf2c51
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-06-29 20:48:03 +0530

    Update Unique Paths 2.cpp

--- Unique Paths 2.cpp
+++ Unique Paths 2.cpp
@@ -40,3 +40,34 @@ public:
         return helper(n-1,m-1,obstacleGrid,dp);
     }
 };
+
+//TABULATION
+//time o(n*m)
+//space o(n*m)
+class Solution {
+public:
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n=obstacleGrid.size();
+        int m=obstacleGrid[0].size();
+        vector<vector<int>> dp(n,vector<int> (m,0));
+        if(obstacleGrid[0][0]==1) return 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                 int up=0,left=0;
+                if(i==0 && j==0) dp[0][0]=1;
+                else if(obstacleGrid[i][j]!=1)
+                {
+                    if(i-1>=0) up=dp[i-1][j];
+                    if(j-1>=0) left=dp[i][j-1];
+                    
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+};
